6e911bab9b5ae8c2aea9610f8ad4f77eefbd2d9a
rose <rosefelix99@gmail.com>
2020-08-28 22:43:56 -0300

    Soma progressiva

---
+++ Entrega/rose-felix/ex5.js
@@ -0,0 +1,18 @@
+const readline = require("readline-sync")
+
+const numero = parseFloat(readline.question("Digite um valor:"))
+
+var i = numero
+
+let valor =0
+
+function calc(numero) {
+    while( i >= 1){
+        valor = valor + i
+        i = i - 1
+        console.log(i)
+    }
+    return console.log("retorno: ", valor)
+}
+
+calc(numero)
